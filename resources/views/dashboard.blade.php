72f1cf359972ffac42ee10d4619c3c4d5d3077d3
virmoc <virmoc411@gmail.com>
2022-05-17 14:26:39 +0200

    dashboard, welcomeblade token

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <meta name="csrf-token" content="{{ csrf_token() }}" /> -->
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <link href="../css/stilus.css" rel="stylesheet" type="text/css" />
